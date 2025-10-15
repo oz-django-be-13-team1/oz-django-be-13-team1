b040d8f0f54dbb80457d969053afd9930ea9b9be
moxopills <moxopills@gmail.com>
2025-10-15 10:42:16 +0900

    🐛 fix: 로그아웃 추가 및 기능 수정

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import CookieTokenObtainPairView, CookieTokenRefreshView, CookieLogoutView
+from .views import (
+    CookieTokenObtainPairView,
+    CookieTokenRefreshView,
+    CookieLogoutView,
+    HeaderLogoutView,
+)
 
 app_name = "accounts"
 urlpatterns = [
     path("cookie/login/",   CookieTokenObtainPairView.as_view(), name="cookie-login"),
-    path("cookie/refresh/", CookieTokenRefreshView.as_view(),    name="cookie-refresh"),
-    path("cookie/logout/",  CookieLogoutView.as_view(),          name="cookie-logout"),
+    path("cookie/refresh/", CookieTokenRefreshView.as_view(), name="cookie-refresh"),
+    path("cookie/logout/",  CookieLogoutView.as_view(), name="cookie-logout"),
+    path("jwt/logut/", HeaderLogoutView.as_view(), name="jwt-logut"),
 ]
