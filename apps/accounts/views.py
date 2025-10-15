54f5f5ec50a60e2b875e3778f6be49e3da6db56e
s4ngmin-9 <s4ngmin9@naver.com>
2025-10-15 13:34:29 +0900

    ✨ feat: AccountsViewSet 구현 (계좌 CRUD API)

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import Accounts
+from .serializers import  AccountsSerializer
 
-# Create your views here.
+# ModelViewSet을 상속받아 CRUD 기능을 한 번에 구현하고자 함
+class AccountsViewSet(viewsets.ModelViewSet):
+    queryset = Accounts.objects.all()
+    serializer_class = AccountsSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Accounts.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        # 삭제 성공 시 204 상태 코드와 함께 사용자 정의 메시지를 반환시킴
+        return Response({"detail": "계좌가 성공적으로 삭제되었습니다."}, status=status.HTTP_204_NO_CONTENT)
