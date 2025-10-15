fa9d2d72d504d08dc334b3f23ca70a38564c7bd4
moxopills <moxopills@gmail.com>
2025-10-15 11:17:03 +0900

    🐛 fix : serializers 코드 수정

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -4,6 +4,22 @@ from .models import User
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True,min_length=15)
 
+    class Meta:
+        model = User
+        fields = ["email","password","nickname","name","phone_number"]
+
+    def create(self,validated_data):
+        password = validated_data.pop('password')
+        user = User(**validated_data)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def validate_email(self,value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("이미 사용 중인 이메일입니다.")
+        return value
+
 class UserReadSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -17,18 +33,6 @@ class UserUpdateSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email','password','nickname','name','phone_number']
         read_only_fields = ['user_id','email','last_login']
-
-    def validate_email(self,value):
-        if User.objects.filter(email=value).exists():
-            raise serializers.ValidationError("이미 사용 중인 이메일입니다.")
-        return value
-
-    def create(self,validated_data):
-        password = validated_data.pop('password')
-        user = User(**validated_data)
-        user.set_password(password)
-        user.save()
-        return user
 
     def update(self,instance,validated_data):
         password = validated_data.pop('password',None)
