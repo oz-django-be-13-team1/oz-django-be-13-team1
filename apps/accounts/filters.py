5b5d048146d155cd2b1c83470aa16fefdc6c2187
s4ngmin-9 <s4ngmin9@naver.com>
2025-10-16 15:42:45 +0900

    💡 chore : (transacction -> transaction) 오타 수정

--- apps/accounts/filters.py
+++ apps/accounts/filters.py
@@ -4,7 +4,7 @@ from .models import Transaction
 class TransactionFilter(django_filters.FilterSet):
     start_date = django_filters.DateFilter(field_name='date', lookup_expr='gte')
     end_date = django_filters.DateTimeFilter(field_name='created_at', lookup_expr='lte')
-    transaction_type = django_filters.CharFilter(field_name = 'transacction_type', lookup_expr = 'iexact')
+    transaction_type = django_filters.CharFilter(field_name = 'transaction_type', lookup_expr = 'iexact')
     min_amount = django_filters.NumberFilter(field_name='amount', lookup_expr='gte')
     max_amount = django_filters.NumberFilter(field_name='amount', lookup_expr='lte')
 
