6c71518b804a500e06eb0d898cd5c68f91e9ae9b
romiii35 <romiii9735@gmail.com>
2025-10-20 16:52:25 +0900

    prod.py 수정완료

---
+++ config/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+from dotenv import load_dotenv
+
+load_dotenv(BASE_DIR/"envs/.env")
+
+ALLOWED_HOSTS = ["13.209.97.126", "0.0.0.0"]
+
+DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "HOST": os.getenv("POSTGRES_HOST", default="db"),
+            "PORT": os.getenv("POSTGRES_PORT", default="5432"),
+        }
+    }
