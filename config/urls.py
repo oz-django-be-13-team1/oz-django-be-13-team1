5958f21cc99e6a74700a44f7ca96d6f3905ad38c
moxopills <moxopills@gmail.com>
2025-10-20 13:55:12 +0900

    🐛 fix : swagger 수정

--- config/urls.py
+++ config/urls.py
@@ -3,16 +3,16 @@ from django.urls import path, include, re_path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from django.shortcuts import redirect
+from rest_framework import permissions
 
 schema_view = get_schema_view(
     openapi.Info(
         title="MY API",
         default_version='v1',
-        description="문서 설명 입니다.",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="example@gmail.com"),
-        license=openapi.License(name="BSD License"),
-    )
+        description="API 문서입니다",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),  # 로그인 없이 접근
 )
 
 urlpatterns = [
